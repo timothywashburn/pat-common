95815ba939283ae2f1978aa1fb7a6ac3d57a3b55
Timothy <timothyrwashburn@gmail.com>
2025-06-15 15:19:27 -0700

    from date string method

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -3,3 +3,7 @@ import { DateString } from "../types";
 export const toDateString = (date: Date): DateString => {
     return date.toISOString() as DateString;
 }
+
+export const fromDateString = (dateString: DateString): Date => {
+    return new Date(dateString);
+}

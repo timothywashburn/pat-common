2d8861a2604e35b4eab669e343de38fe96fc400b
Timothy <timothyrwashburn@gmail.com>
2025-06-18 01:00:46 -0700

    update other props 2

--- src/types/misc-types.ts
+++ src/types/misc-types.ts
@@ -5,7 +5,8 @@ export type DateOnlyString = string & { readonly __brand: "DateOnlyString" };
 export const dateOnlyStringSchema = z.string()
     .refine(val => /^\d{4}-\d{2}-\d{2}$/.test(val), {
         message: "Invalid date-only string format, expected YYYY-MM-DD",
-    });
+    })
+    .transform(val => val as DateOnlyString);
 
 export type DateString = string & { readonly __brand: "DateString" };
 

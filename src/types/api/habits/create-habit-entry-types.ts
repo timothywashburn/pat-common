494294130130079e241bfe59c7cfb48734f281dc
Timothy <timothyrwashburn@gmail.com>
2025-06-19 21:49:23 -0700

    improve db update calls 6

--- src/types/api/habits/create-habit-entry-types.ts
+++ src/types/api/habits/create-habit-entry-types.ts
@@ -1,16 +1,14 @@
 import { z } from "zod";
 import { Habit, HabitEntryStatus } from "../../models";
 import { DateOnlyString, dateOnlyStringSchema } from "../../misc-types";
+import { createHabitRequestSchema } from "./create-habit-types";
 
 export const createHabitEntryRequestSchema = z.object({
     date: dateOnlyStringSchema,
     status: z.nativeEnum(HabitEntryStatus)
 });
 
-export interface CreateHabitEntryRequest {
-    date: DateOnlyString;
-    status: HabitEntryStatus;
-}
+export type CreateHabitEntryRequest = z.infer<typeof createHabitEntryRequestSchema>;
 
 export interface CreateHabitEntryResponse {
     habit: Habit;
--- src/types/api/habits/create-habit-types.ts
+++ src/types/api/habits/create-habit-types.ts
@@ -1,21 +1,16 @@
 import { z } from "zod";
-import { Habit } from "../../models/habit-data";
+import { Habit, HabitFrequency } from "../../models/habit-data";
+import { createAccountRequestSchema } from "../auth";
 
 export const createHabitRequestSchema = z.object({
     name: z.string().min(1, 'Name is required').trim(),
     description: z.string().trim().optional(),
     notes: z.string().trim().optional(),
-    frequency: z.literal('daily'),
+    frequency: z.nativeEnum(HabitFrequency),
     rolloverTime: z.string().regex(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/, 'rolloverTime must be in HH:MM format')
 });
 
-export interface CreateHabitRequest {
-    name: string;
-    description?: string;
-    notes?: string;
-    frequency: 'daily';
-    rolloverTime: string;
-}
+export type CreateHabitRequest = z.infer<typeof createHabitRequestSchema>;
 
 export interface CreateHabitResponse {
     habit: Habit;

3f728c3cf6598a24a672ccc4a4021e4a3c8e0957
Timothy <timothyrwashburn@gmail.com>
2025-07-01 19:11:00 -0700

    fix nullish create item types

--- src/types/api/items/create-item-types.ts
+++ src/types/api/items/create-item-types.ts
@@ -3,11 +3,11 @@ import { ItemData, Serialized } from "../../models";
 
 export const createItemRequestSchema = z.object({
     name: z.string().min(1),
-    dueDate: z.string().nullish(),
+    dueDate: z.string().optional(),
     notes: z.string().optional(),
     urgent: z.boolean().optional().default(false),
-    category: z.string().nullish(),
-    type: z.string().nullish()
+    category: z.string().optional(),
+    type: z.string().optional()
 });
 
 export type CreateItemRequest = z.infer<typeof createItemRequestSchema>;

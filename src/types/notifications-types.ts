52485bc75668bf2aca9e348a7890a2ad9a42287e
Timothy <timothyrwashburn@gmail.com>
2025-08-24 17:50:34 -0700

    notifications v4 data structure 4

--- src/types/notifications-types.ts
+++ src/types/notifications-types.ts
@@ -28,6 +28,7 @@ export enum NotificationSchedulerType {
 
 export enum NotificationVariantType {
     AGENDA_ITEM_UPCOMING_DEADLINE = 'agenda_item_upcoming_deadline',
+    HABIT_UPCOMING_DEADLINE = 'habit_upcoming_deadline',
 }
 
 export const notificationSchedulerDataSchema = z.discriminatedUnion('type', [
